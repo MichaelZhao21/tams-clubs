8529c96e419c5c1d0aade8c0f3a8e3d615c36de3
zach <zachtang0123@gmail.com>
2020-06-16 23:23:45 -0500

    Updates

---
+++ precache-manifest.5714d08e5535bcc509e9de3aee18235b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abd2e741a36076863bd95d0926c014e",
+    "url": "/tams-clubs/index.html"
+  },
+  {
+    "revision": "64d40ffe760ec73e6aeb",
+    "url": "/tams-clubs/static/css/main.0381aabc.chunk.css"
+  },
+  {
+    "revision": "fbda36fb5427fd916d9c",
+    "url": "/tams-clubs/static/js/2.edd5600c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tams-clubs/static/js/2.edd5600c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d40ffe760ec73e6aeb",
+    "url": "/tams-clubs/static/js/main.73846592.chunk.js"
+  },
+  {
+    "revision": "511f2a59a4539dcacb29",
+    "url": "/tams-clubs/static/js/runtime-main.edbe2cd3.js"
+  }
+]);
